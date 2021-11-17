ccbda5d398b8b88b7dd7d732ae9ac32b422340cc
= <=>
2021-11-16 21:30:37 -0300

    Criando um arquivo de testes para ler arquivos .csv

---
+++ tests.py
@@ -0,0 +1,9 @@
+import csv
+import glob
+dados = []
+arquivos = glob.glob("./pacientes/*.csv")
+for arquivo in arquivos:
+    with open(arquivo,"r") as leitura:
+        reader = csv.reader(leitura)
+        for linha in reader:
+            print(linha)
