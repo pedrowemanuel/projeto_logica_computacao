1ec174ec3d892dd2ee58d08f8d54b0f33f0077ec
= <=>
2021-11-16 21:39:17 -0300

    Criando um arquivo de testes para ler arquivos .csv

--- tests.py
+++
@@ -1,9 +0,0 @@
-import csv
-import glob
-dados = []
-arquivos = glob.glob("./pacientes/*.csv")
-for arquivo in arquivos:
-    with open(arquivo,"r") as leitura:
-        reader = csv.reader(leitura)
-        for linha in reader:
-            print(linha)
