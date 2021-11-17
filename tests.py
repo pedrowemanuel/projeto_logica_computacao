664ab5ec82864ab0f2b99c09421c9094e296e2f9
= <=>
2021-11-16 21:43:06 -0300

    Arquivo de testes para o csv

--- tests.py
+++ tests.py
@@ -6,5 +6,4 @@ for arquivo in arquivos:
     with open(arquivo,"r") as leitura:
         reader = csv.reader(leitura)
         for linha in reader:
-            dados.append(linha)
-print(dados)
+            print(linha)
