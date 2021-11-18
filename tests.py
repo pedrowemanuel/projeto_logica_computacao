4f1e7278836d1e5dd342ba340ee5c98de2d5c457
= <=>
2021-11-18 20:52:12 -0300

    Calculando a quantidade de linhas e colunas que cada arquivo contera

--- tests.py
+++ tests.py
@@ -1,9 +1,12 @@
 import csv
 import glob
-dados = []
+alfabeto = ['a','b',"c",'d','e','f',"g",'h','i','j',"k",'l','m','n',"o",'p','q','r','s','t','u','v','w','x','y','z',".","_"]
 arquivos = glob.glob("./pacientes/*.csv")
 for arquivo in arquivos:
+    file = arquivo.split("_")
+    pacientes = int("".join(x for x in file[3] if x not in alfabeto))
+    atributos = int("".join(x for x in file[2] if x not in alfabeto))
     with open(arquivo,"r") as leitura:
         reader = csv.reader(leitura)
-        for linha in reader:
-            print(linha)
+        for arq in reader:
+            print(arq)
