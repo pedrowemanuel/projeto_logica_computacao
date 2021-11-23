3b9402b1e6390ab4509a6d3b277d8f3ff4976c35
pedrowemanuel <pedronuel36@gmail.com>
2021-11-23 11:57:45 -0300

    Implementado restrição 1: apenas_tres_casos_para_cada_regra_atributo
    Implementado restrição 3:
    pacientes_sem_patologia_algum_atributo_nao_aplicado_regra

--- restrictions.py
+++ restrictions.py
@@ -3,6 +3,40 @@ e montem as fórmulas que representem as restrições para a classificação de 
 
 from lib.semantics import *
 from lib.functions import *
+
+
+"""Restrição 1: Para cada atributo e cada regra, temos exatamente uma das três possibilidades: o atributo aparece
+positivamente na regra, o atributo aparece negativamente na regra ou o atributo não aparece na regra."""
+def apenas_tres_casos_para_cada_regra_atributo(numero_de_regras, atributos):
+    dados_formula = []
+    for i in range(1, numero_de_regras + 1):
+        and_lista = []
+
+        for a in range(len(atributos)):
+            and_lista.append(
+                Or (
+                    Or(
+                        And(
+                            And(Atom('x_' + str(atributos[a]) + '_' + str(i) + '_p'), Not(Atom('x_' + str(atributos[a]) + '_' + str(i) + '_n'))),
+                            Not(Atom('x_' + str(atributos[a]) + '_' + str(i) + '_s'))
+                        ),
+                        And(
+                            And(Not(Atom('x_' + str(atributos[a]) + '_' + str(i) + '_p')), Atom('x_' + str(atributos[a]) + '_' + str(i) + '_n')),
+                            Not(Atom('x_' + str(atributos[a]) + '_' + str(i) + '_s'))
+                        ),
+                    ),
+                    And(
+                        And(Not(Atom('x_' + str(atributos[a]) + '_' + str(i) + '_p')), Not(Atom('x_' + str(atributos[a]) + '_' + str(i) + '_n'))),
+                        Atom('x_' + str(atributos[a]) + '_' + str(i) + '_s')
+                    ),
+                )
+            )
+    
+        formula_or = or_all(and_lista)
+        dados_formula.append(formula_or)
+
+    return and_all(dados_formula)
+
 '''Cada regra deve ter algum atributo aparecendo nela'''
 def restricao2(atributos , num_regras):
     dados_formula = []
@@ -23,6 +57,31 @@ def restricao2(atributos , num_regras):
         dados_formula.append(formula)
     return and_all(dados_formula)
 
+"""Restrição 3: Para cada paciente sem patologia e cada regra, algum atributo do paciente não pode ser aplicado à
+regra."""
+def pacientes_sem_patologia_algum_atributo_nao_aplicado_regra(pacientes_sem_patologia, numero_de_regras, atributos):
+    dados_formula = []
+
+    for j in range(len(pacientes_sem_patologia)):
+        and_lista = []
+
+        for i in range(1, numero_de_regras + 1):
+            or_lista = []
+
+            for a in range(len(pacientes_sem_patologia[j])):
+                if pacientes_sem_patologia[j][a] == 0:
+                    or_lista.append(Atom('x_' + str(atributos[a]) + '_' + str(i) + '_p'))
+                elif pacientes_sem_patologia[j][a] == 1:
+                    or_lista.append(Atom('x_' + str(atributos[a]) + '_' + str(i) + '_n'))
+        
+            formula_or = or_all(or_lista)
+            and_lista.append(formula_or)
+
+        formula_and = and_all(and_lista)
+        dados_formula.append(formula_and)
+
+    return and_all(dados_formula)
+
 """Restrição 5: Cada paciente com patologia deve ser coberto por alguma das regras."""
 def pacientes_com_patologia_cobertos_alguma_regra(pacientes_com_patologia, numero_de_regras):
 
@@ -31,7 +90,7 @@ def pacientes_com_patologia_cobertos_alguma_regra(pacientes_com_patologia, numer
     for j in range(len(pacientes_com_patologia)):
         or_lista = []
 
-        for i in range(1, numero_de_regras):
+        for i in range(1, numero_de_regras + 1):
             or_lista.append(Atom('c_'+ str(i) + '_' + str(j + 1)))
 
         formula_or = or_all(or_lista)
