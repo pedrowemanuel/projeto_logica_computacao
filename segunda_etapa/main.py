536aaf8bf256a51293946bc052f0f6511b964d59
pedrowemanuel <pedronuel36@gmail.com>
2022-01-31 22:39:40 -0300

    Ajustes

--- segunda_etapa/main.py
+++ segunda_etapa/main.py
@@ -3,7 +3,8 @@ from lib.functions import *
 from restrictions import *
 
 def main(args):
-
+    formula = Or(Atom('p'), Not(Not('q')))
+    print(is_cnf(formula))
     return 0 
 
 if __name__ == "__main__":
